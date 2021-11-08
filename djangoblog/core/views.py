3a28ab534d02713f12b279ce80ed67e221dcf3da
tarunkumarnahak25 <tarunnaha25@gmail.com>
2021-11-08 21:01:22 +0530

    added category

---
+++ djangoblog/core/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from blog.models import Post
+
+def frontpage(request):
+    posts = Post.objects.all()
+    return render(request, 'core/frontpage.html', {"posts": posts})
+
+
+def aboutpage(request):
+    return render(request, 'core/about.html') 
+
