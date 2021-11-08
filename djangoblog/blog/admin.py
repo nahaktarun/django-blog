0fa51fbb4215594189d71432af5d66a8f1ff8cc6
tarunkumarnahak25 <tarunnaha25@gmail.com>
2021-11-08 21:35:01 +0530

    added category

--- djangoblog/blog/admin.py
+++ djangoblog/blog/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Post
+from .models import Post, Category, Comment
 
 admin.site.register(Post)
+
+admin.site.register(Category)
+
+admin.site.register(Comment)
--- djangoblog/blog/models.py
+++ djangoblog/blog/models.py
@@ -6,9 +6,14 @@ class Category(models.Model):
     
     class Meta:
         ordering = ('title',)
+        verbose_name_plural = 'Categories'
+        
+    def __str__(self):
+        return self.title
 # Create your models here.
 
 class Post(models.Model):
+    category = models.ForeignKey(Category, related_name= 'posts', on_delete= models.CASCADE)
     title = models.CharField(max_length = 255)
     slug = models.SlugField()
     intro = models.TextField()
@@ -18,7 +23,8 @@ class Post(models.Model):
     
     class Meta:
         ordering = ('-created_at',)
-        
+    def __str__(self):
+        return self.title    
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete= models.CASCADE)
