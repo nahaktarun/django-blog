c508bc1629d7bf437ff76cd88036c86145b05782
tarunkumarnahak25 <tarunnaha25@gmail.com>
2021-11-08 22:39:23 +0530

    added draft and active feature in post as well as in category

--- djangoblog/blog/views.py
+++ djangoblog/blog/views.py
@@ -4,7 +4,7 @@ from .models import Category, Post
 # Create your views here.
 
 def detail(request,category_slug, slug):
-    post = get_object_or_404(Post, slug = slug)
+    post = get_object_or_404(Post, slug = slug, status = Post.ACTIVE)
     
     if request.method == 'POST':
         form = CommentForm(request.POST)
@@ -24,5 +24,5 @@ def detail(request,category_slug, slug):
 
 def category(request, slug):
     category = get_object_or_404(Category, slug = slug)
-    
-    return render(request,'blog/category.html', {"category": category})
+    posts = category.posts.filter(status =  Post.ACTIVE)
+    return render(request,'blog/category.html', {"category": category, 'posts':posts})
